7cc155e7b2df910e037761867b7f0ff49dadabb8
Seraphino16 <seraphin.benoit@epitech.eu>
2023-12-15 12:39:37 +0100

    add recursive

--- css_generator.php
+++ css_generator.php
@@ -1,21 +1,59 @@
 #!/usr/bin/php
 <?php
 
+// array_shift($argv);
+// print_r($argv);
+
+$sprite_name = "sprite.png";
+$css_name = "sprite.css";
+
+for($i = 1; $i < $argc; $i++) {
+    if(strpos($argv[$i], "-") === 0) {
+        switch($argv[$i]) {
+            case "-r":
+                echo "Récusivité activée\n";
+                break;
+            case "-i":
+                echo "Choose your sprite name\n";
+                break;
+            case "-s":
+                echo "Choose your css name\n";
+                break;
+            default:
+                echo "Error\n";
+        }
+    }
+}
+
+
 function my_scandir ($dir_path)
 {
     $my_pngs = array();
 
-    $dir_name = basename($dir_path);
+    // $dir_name = basename($dir_path);
 
     if($handle = opendir($dir_path)) {
         // echo "On a trouvé dans $dir_name:\n";
+        
 
-        while(false !== $img_name = readdir($handle)) {
-            if(preg_match("/.+\.png$/", $img_name)) {
-                array_push($my_pngs, "$dir_path/$img_name");
-                echo $img_name . "\n";
+        while(false !== $file_name = readdir($handle)) {
+            // echo $file_name . "\n";
+            $file_path = "$dir_path/$file_name";
+            if(preg_match("/.+\.png$/", $file_name)) {
+                // echo "pregmatch\n";
+                array_push($my_pngs, $file_path);
+                // echo "fichier = $file_name" . "\n";
+            } elseif (false !== is_dir($file_path)) {
+                // echo "dossier = $file_name" . "\n";
+                if($file_name !== "." && $file_name !== "..") {
+                    // echo $file_name . "\n";
+                    $my_pngs = array_merge(my_scandir($file_path), $my_pngs);
+                    
+                    // print_r($my_pngs);
+                }
+                
             }
-        }
+        } 
     }
 
     return $my_pngs;
@@ -26,6 +64,7 @@ function my_merge_image ($my_array)
     $sprite_width = 0;
     $sprite_height = 0;
 
+    //la première boucle sert à créer le sprite aux bonnes dimensions
     foreach($my_array as $img_path) {
         $img = imagecreatefrompng($img_path);
         $sprite_width += imagesx($img);
@@ -34,6 +73,7 @@ function my_merge_image ($my_array)
 
     $sprite = imagecreatetruecolor($sprite_width, $sprite_height);
 
+    //on crée le fichier css et on met la classe "sprite" à l'intérieur
     $file_content = ".sprite {\n";
     $file_content .= "\tbackground-image: url(sprite.png);\n";
     $file_content .= "\tbackground-repeat: no-repeat;\n";
@@ -43,9 +83,11 @@ function my_merge_image ($my_array)
         
     file_put_contents("style.css", $file_content);
 
+    //on initie les variables de position
     $x_pos = 0;
     $y_pos = 0;
 
+    //dans chaque boucle on crée un image qu'on copie dans le sprite après avoir récupéré la tailles et la position
     foreach($my_array as $img_path) {
         $img = imagecreatefrompng($img_path);
 
@@ -54,6 +96,8 @@ function my_merge_image ($my_array)
 
         imagecopy($sprite, $img, $x_pos, $y_pos, 0, 0, $img_width, $img_height);
 
+
+        //ici on crée la classe de chaque image et on l'ajoute au css
         $img_name = basename($img_path);
 
         $class_css = ".$img_name {
@@ -64,6 +108,7 @@ function my_merge_image ($my_array)
 
         file_put_contents("style.css", $class_css, FILE_APPEND);
 
+        //on incrémente la positon x pour que les images ne se superposent pas
         $x_pos += $img_width;
     }
 
@@ -77,6 +122,7 @@ if($folder_path === "man") {
     echo file_get_contents("manual_css_generator.txt");
 } else {
     $arr = my_scandir($folder_path);
+    echo "Les fichiers .png ont été récupérés\n";
     my_merge_image($arr);
 }
 
