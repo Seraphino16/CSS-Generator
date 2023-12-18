80a3810dad2c63d324d7cd8b8210d227050b69cf
Séraphin <seraphinbnt@gmail.com>
2023-12-18 15:25:04 +0100

    split fonction into smaller ones

--- my_merge.php
+++ my_merge.php
@@ -1,9 +1,11 @@
 <?php
 
-function my_merge_image ($my_array, $sprite_name, $css_name)
+$sprite_width = 0;
+$sprite_height = 0;
+
+function create_transparent_img ($my_array)
 {
-    $sprite_width = 0;
-    $sprite_height = 0;
+    global $sprite_width, $sprite_height;
 
     //la première boucle sert à créer le sprite aux bonnes dimensions
     foreach($my_array as $img_path) {
@@ -13,17 +15,13 @@ function my_merge_image ($my_array, $sprite_name, $css_name)
     }
 
     $sprite = imagecreatetruecolor($sprite_width, $sprite_height);
+    $black = imagecolorallocate($sprite, 0, 0, 0);
+    imagecolortransparent($sprite, $black);
+    return $sprite;
+}
 
-    //on crée le fichier css et on met la classe "sprite" à l'intérieur
-    $file_content = ".sprite {\n";
-    $file_content .= "\tbackground-image: url(sprite.png);\n";
-    $file_content .= "\tbackground-repeat: no-repeat;\n";
-    $file_content .= "\tdisplay: block;\n";
-    $file_content .= "\twidth: $sprite_width" . "px;\n";
-    $file_content .= "\theight: $sprite_height" . "px;\n}\n\n";
-        
-    file_put_contents($css_name, $file_content);
-
+function copy_image_and_create_css ($my_array, $sprite, $css_name)
+{
     //on initie les variables de position
     $x_pos = 0;
     $y_pos = 0;
@@ -36,7 +34,6 @@ function my_merge_image ($my_array, $sprite_name, $css_name)
         $img_height = imagesy($img);
 
         imagecopy($sprite, $img, $x_pos, $y_pos, 0, 0, $img_width, $img_height);
-
 
         //ici on crée la classe de chaque image et on l'ajoute au css
         $img_name = basename($img_path);
@@ -52,6 +49,25 @@ function my_merge_image ($my_array, $sprite_name, $css_name)
         //on incrémente la positon x pour que les images ne se superposent pas
         $x_pos += $img_width;
     }
+}
 
+function my_merge_image ($my_array, $sprite_name, $css_name)
+{
+    global $sprite_width, $sprite_height;
+
+    $sprite = create_transparent_img($my_array);
+
+    //on crée le fichier css et on met la classe "sprite" à l'intérieur
+    $file_content = ".sprite {\n";
+    $file_content .= "\tbackground-image: url(sprite.png);\n";
+    $file_content .= "\tbackground-repeat: no-repeat;\n";
+    $file_content .= "\tdisplay: block;\n";
+    $file_content .= "\twidth: $sprite_width" . "px;\n";
+    $file_content .= "\theight: $sprite_height" . "px;\n}\n\n";
+        
+    file_put_contents($css_name, $file_content);
+
+    copy_image_and_create_css($my_array, $sprite, $css_name);
+    
     return imagepng($sprite, $sprite_name);
 }
--- my_scandir.php
+++ my_scandir.php
@@ -4,31 +4,22 @@ function my_scandir ($dir_path, $is_recursive)
 {
     $my_pngs = array();
 
-    // $dir_name = basename($dir_path);
-
     if($handle = opendir($dir_path)) {
-        // echo "On a trouvé dans $dir_name:\n";
         
-
         while(false !== $file_name = readdir($handle)) {
-            // echo $file_name . "\n";
             $file_path = "$dir_path/$file_name";
             if(preg_match("/.+\.png$/", $file_name)) {
-                // echo "pregmatch\n";
                 array_push($my_pngs, $file_path);
-                // echo "fichier = $file_name" . "\n";
+                // echo "image = $file_name" . "\n";
             } elseif (false !== is_dir($file_path) && false !== $is_recursive) {
-                // echo "dossier = $file_name" . "\n";
-                if($file_name !== "." && $file_name !== "..") {
-                    // echo $file_name . "\n";
-                    $my_pngs = array_merge(my_scandir($file_path, $is_recursive), $my_pngs);
-                    
-                    // print_r($my_pngs);
-                }
                 
+                if($file_name !== "." && $file_name !== "..") {
+                    $my_pngs = array_merge(my_scandir($file_path, $is_recursive), $my_pngs);  
+                    // echo "dossier = $file_name" . "\n";                  
+                }
             }
         } 
     }
-
+    // print_r($my_pngs);
     return $my_pngs;
 }
