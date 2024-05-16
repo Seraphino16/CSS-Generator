e497ade1384dec2149fa0056008591e5b0faf2d0
Séraphin <seraphinbnt@gmail.com>
2024-05-16 18:16:39 +0200

    remove useless comments

--- my_scandir.php
+++ my_scandir.php
@@ -10,16 +10,13 @@ function my_scandir ($dir_path, $is_recursive)
             $file_path = "$dir_path/$file_name";
             if(preg_match("/.+\.png$/", $file_name)) {
                 array_push($my_pngs, $file_path);
-                // echo "image = $file_name" . "\n";
             } elseif (false !== is_dir($file_path) && false !== $is_recursive) {
                 
                 if($file_name !== "." && $file_name !== "..") {
-                    $my_pngs = array_merge(my_scandir($file_path, $is_recursive), $my_pngs);  
-                    // echo "dossier = $file_name" . "\n";                  
+                    $my_pngs = array_merge(my_scandir($file_path, $is_recursive), $my_pngs);               
                 }
             }
         } 
     }
-    // print_r($my_pngs);
     return $my_pngs;
 }
--- options.php
+++ options.php
@@ -4,26 +4,18 @@ $short_options = "r"; //option pour la récusivité
 $short_options .= "i:"; // option pour le nom du sprite / les deux points sont pour la valeur requise
 $short_options .= "s:"; //option pour le nom du css
 $short_options .= "h"; // option pour afficher l'aide
-// $short_options .= "m";
 
 $long_options = array(
     "recursive",
     "output-image:",
     "output-style:",
-    // "man"
 );
 
 $options = getopt($short_options, $long_options);
-// print_r($options);
 
-// if(isset($options["m"])) {
-//     echo file_get_contents("manual_css_generator.txt");
-//     exit(42);
-// }
 $help = (isset($options["h"])) ? true : false;
 
 $is_recursive = (isset($options["r"])) ? true : false;
-// var_dump($is_recursive);
 
 $sprite_name = (isset($options["i"])) ? $options["i"] : "sprite.png";
 $css_name = (isset($options["s"])) ? $options["s"] : "sprite.css";
